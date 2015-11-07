6e143f13bd133db98e6ca5a88553dda03336e1a5
Jason <quizathaderat@yahoo.com>
2015-11-06 17:57:35 -0800

    Add display of total eaten calories

--- client/components/food/FoodBox.jsx
+++ client/components/food/FoodBox.jsx
@@ -4,7 +4,7 @@ import MealList from './MealList';
 
 export default React.createClass({
   getInitialState() {
-    return {data: [], foods: [], meals: []};
+    return {data: [], foods: [], meals: [], tCals: 0};
   },
   componentDidMount() {
     this.readFoodsFromAPI();
@@ -36,17 +36,26 @@ export default React.createClass({
       meals.unshift(meal);
       this.setState({meals: meals});
       this.setState({foods: []})
+      let totalCals = this.state.tCals;
+      totalCals += meal.food.calories;
+      this.setState({tCals: totalCals});
     }.bind(this));
   },
   deleteMeal(data, id) {
     this.props.writeToAPI('delete', this.props.origin + "/meals/" + id, data, (meals) => {
+      let totalCals = 0;
+      meals.forEach(meal => {
+        totalCals += meal.food.calories;
+      });
+      this.setState({tCals: totalCals});
       this.setState({meals: meals});
     })
   },
   render() {
     return (
       <div>
-        <h3>foods</h3>
+        <h2>Foods</h2>
+        <h3>Calories eaten today: {this.state.tCals}</h3>
         <input type='text' placeholder='Search for a food:' className='form-control' onKeyUp={this.onKeyUp} ref='foodSearch' id='foodSearch'/>
         <FoodList data={this.state.foods} writeToAPI={this.props.writeToAPI} origin={this.props.origin} input={this.refs.foodSearch} createMeal={this.createMeal} />
         <MealList data={this.state.meals} deleteMeal={this.deleteMeal} />
