9b9faedad3fc3d1e17d93ebc376f34b791a2177c
Jason <quizathaderat@yahoo.com>
2015-11-12 16:22:10 -0800

    Add d3

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,9 +4,23 @@ import {Link} from 'react-router';
 import FoodBox from './food/FoodBox';
 import ExerciseBox from './exercise/ExerciseBox';
 import Reqwest from 'reqwest';
+
+
+import Chart from './Chart';
+
+var sampleData = [
+  {id: '5fbmzmtc', x: 7, y: 41, z: 6},
+  {id: 's4f8phwm', x: 11, y: 45, z: 9},
+];
 export default React.createClass({
   getDefaultProps() {
     return { origin: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : ''};
+  },
+  getInitialState() {
+    return {
+      data: sampleData,
+      domain: {x: [0, 30], y: [0, 100]}
+    }
   },
   readFromAPI(url, successFunction) {
     Reqwest({
@@ -46,6 +60,7 @@ export default React.createClass({
         </Nav>
       </Navbar>
       <div className='container'>
+        <Chart data={this.state.data} domain={this.state.domain} />
         {childrenWithProps}
       </div>
     </div>;
